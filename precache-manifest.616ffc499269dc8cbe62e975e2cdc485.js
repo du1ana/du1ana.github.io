82263c91ee810c9a950eabe3d631a668dc27ddf2
Dulana Peiris <dulthar@gmail.com>
2020-09-04 12:57:58 +0530

    Updates

---
+++ precache-manifest.616ffc499269dc8cbe62e975e2cdc485.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d0b07c8773d92ce88d07ac81be1385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06605a5a3f613c09c7e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "92191d6619b5f8528d43",
+    "url": "/static/css/main.4ac6099c.chunk.css"
+  },
+  {
+    "revision": "d06605a5a3f613c09c7e",
+    "url": "/static/js/2.636a2e5f.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.636a2e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92191d6619b5f8528d43",
+    "url": "/static/js/main.34277550.chunk.js"
+  },
+  {
+    "revision": "443e46ba3faf8f5be86f",
+    "url": "/static/js/runtime-main.8f1f8863.js"
+  },
+  {
+    "revision": "52f7706df5574419dc2faf98ea83ffe8",
+    "url": "/static/media/code.52f7706d.svg"
+  },
+  {
+    "revision": "638acce7051ea0cbabb6d1f52d9a8e20",
+    "url": "/static/media/coffee.638acce7.svg"
+  },
+  {
+    "revision": "2494fc21dec2ff76fec9f3945091c71f",
+    "url": "/static/media/du.2494fc21.svg"
+  },
+  {
+    "revision": "ef18d6a950128b8d1fda3e0d4ffd73d1",
+    "url": "/static/media/dulana.ef18d6a9.svg"
+  },
+  {
+    "revision": "f977998e23434dcab04b05a3cb0ee709",
+    "url": "/static/media/star.f977998e.svg"
+  }
+]);
